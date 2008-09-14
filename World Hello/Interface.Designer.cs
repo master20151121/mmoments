3ca0f08cf613c3ca38e02245d1020a2113108884
supercars_7 <supercars_7@hotmail.com>
2008-09-14 07:22:12 +0000

    Playing with UI:

--- World Hello/Interface.Designer.cs
+++ World Hello/Interface.Designer.cs
@@ -6,7 +6,6 @@ namespace World_Hello
         /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
-        private System.Windows.Forms.MainMenu mainMenu1;
 
         /// <summary>
         /// Clean up any resources being used.
@@ -30,38 +29,14 @@ namespace World_Hello
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Interface));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.UI_recordBtn = new System.Windows.Forms.Button();
             this.mainMenu1 = new System.Windows.Forms.MainMenu();
             this.menuItem1 = new System.Windows.Forms.MenuItem();
             this.menuItem2 = new System.Windows.Forms.MenuItem();
-            this.button1 = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.UI_progressBar = new System.Windows.Forms.ProgressBar();
+            this.UI_stopBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // mainMenu1
-            // 
-            this.mainMenu1.MenuItems.Add(this.menuItem1);
-            this.mainMenu1.MenuItems.Add(this.menuItem2);
-            // 
-            // menuItem1
-            // 
-            this.menuItem1.Text = "Click Me";
-            this.menuItem1.Click += new System.EventHandler(this.menuItem1_Click);
-            // 
-            // menuItem2
-            // 
-            this.menuItem2.Text = "Exit";
-            this.menuItem2.Click += new System.EventHandler(this.menuItem2_Click);
-            // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.SystemColors.Desktop;
-            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.button1.Location = new System.Drawing.Point(37, 17);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(79, 32);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Click Me";
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // pictureBox1
             // 
@@ -72,13 +47,56 @@ namespace World_Hello
             this.pictureBox1.Size = new System.Drawing.Size(240, 294);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             // 
+            // UI_recordBtn
+            // 
+            this.UI_recordBtn.BackColor = System.Drawing.Color.LawnGreen;
+            this.UI_recordBtn.Location = new System.Drawing.Point(3, 3);
+            this.UI_recordBtn.Name = "UI_recordBtn";
+            this.UI_recordBtn.Size = new System.Drawing.Size(70, 30);
+            this.UI_recordBtn.TabIndex = 2;
+            this.UI_recordBtn.Text = "Record";
+            this.UI_recordBtn.Click += new System.EventHandler(this.UI_recordBtn_Click);
+            // 
+            // mainMenu1
+            // 
+            this.mainMenu1.MenuItems.Add(this.menuItem1);
+            this.mainMenu1.MenuItems.Add(this.menuItem2);
+            // 
+            // menuItem1
+            // 
+            this.menuItem1.Text = "Click Me";
+            // 
+            // menuItem2
+            // 
+            this.menuItem2.Text = "Exit";
+            // 
+            // UI_progressBar
+            // 
+            this.UI_progressBar.Location = new System.Drawing.Point(36, 139);
+            this.UI_progressBar.Name = "UI_progressBar";
+            this.UI_progressBar.Size = new System.Drawing.Size(164, 20);
+            this.UI_progressBar.Visible = false;
+            // 
+            // UI_stopBtn
+            // 
+            this.UI_stopBtn.BackColor = System.Drawing.Color.Red;
+            this.UI_stopBtn.Location = new System.Drawing.Point(3, 3);
+            this.UI_stopBtn.Name = "UI_stopBtn";
+            this.UI_stopBtn.Size = new System.Drawing.Size(72, 30);
+            this.UI_stopBtn.TabIndex = 5;
+            this.UI_stopBtn.Text = "Stop";
+            this.UI_stopBtn.Visible = false;
+            this.UI_stopBtn.Click += new System.EventHandler(this.UI_stopBtn_Click);
+            // 
             // Interface
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.AutoScroll = true;
             this.ClientSize = new System.Drawing.Size(240, 294);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.UI_stopBtn);
+            this.Controls.Add(this.UI_progressBar);
+            this.Controls.Add(this.UI_recordBtn);
             this.Controls.Add(this.pictureBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.KeyPreview = true;
@@ -94,10 +112,13 @@ namespace World_Hello
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button UI_recordBtn;
+        private System.Windows.Forms.MainMenu mainMenu1;
         private System.Windows.Forms.MenuItem menuItem1;
         private System.Windows.Forms.MenuItem menuItem2;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ProgressBar UI_progressBar;
+        private System.Windows.Forms.Button UI_stopBtn;
     }
 }
 
