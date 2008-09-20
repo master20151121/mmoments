cacfb5a3cbdc1d5bb17c99cafc84f8acbde1a61a
freelancer715@yahoo.com.au <freelancer715@yahoo.com.au@11f555aa-7f0e-11dd-8a74-1916a93a1be7>
2008-09-20 21:45:46 +0000

    added more code for backend

--- trunk/databaseserver/databaseserver/Program.cs
+++ trunk/databaseserver/databaseserver/Program.cs
@@ -1,6 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+using System.Threading;
 
 namespace databaseserver
 {
@@ -8,6 +12,37 @@ namespace databaseserver
     {
         static void Main(string[] args)
         {
+            Thread listener = new Thread(ListenForConnections);
+            listener.Name = "ConnectionListener";
+            listener.IsBackground = true;
+            listener.Start();
+            Console.In.ReadLine();
+        }
+        static void ListenForConnections()
+        {
+            TcpListener ss = new TcpListener(345);
+            ss.Start();
+            Random r = new Random();
+            Socket s;
+            while (true)
+            {
+                s = ss.AcceptSocket();
+                Thread conn = new Thread(new ParameterizedThreadStart(Connection));
+                conn.Name = "mmomentsconnection" + conn.ManagedThreadId;
+                conn.IsBackground = true;
+                conn.Start(s);
+            }
+        }
+        static void Connection(object s_)
+        {
+            Socket s = (Socket)s_;
+            NetworkStream ns = new NetworkStream(s);
+            BinaryWriter bw = new BinaryWriter(ns);
+            BinaryReader br = new BinaryReader(ns);
+            StreamReader sr = new StreamReader(ns);
+            bw.Write("Hello I am " + Thread.CurrentThread.Name + '\n');
+            bw.Flush();
+            System.Diagnostics.Debug.WriteLine(sr.ReadLine());
         }
     }
 }
