acce5642237e982e5bf359e7ff40898ddb727986
freelancer715@yahoo.com.au <freelancer715@yahoo.com.au@11f555aa-7f0e-11dd-8a74-1916a93a1be7>
2008-09-21 12:06:09 +0000

    Added some modifications, song data is returned to client in a serialised xml form, providing the a way for the objects to be passed across the network

---
+++ trunk/databaseserver/databaseserver/Song.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace databaseserver
+{
+    [XmlRoot("songList")]
+    public class SongList
+    {
+        private ArrayList listOfSongs;
+
+          public SongList() {
+            listOfSongs = new ArrayList();
+          }
+
+          [XmlElement("song")]
+          public Song[] Songs {
+            get {
+              Song[] songs = new Song[ listOfSongs.Count ];
+              listOfSongs.CopyTo( songs );
+              return songs;
+            }
+            set {
+              if( value == null ) return;
+              Song[] songs = (Song[])value;
+              listOfSongs.Clear();
+              foreach( Song song in songs )
+                listOfSongs.Add( song );
+            }
+          }
+
+          public int Add( Song song ) {
+            return listOfSongs.Add( song );
+          }
+    }
+    public class Song
+    {
+        [XmlAttribute("title")] public string title;
+        [XmlAttribute("artist")] public string artist;
+        [XmlAttribute("match")] public int match;
+        public Song(string title_, string artist_, int match_)
+        {
+            title = title_;
+            artist = artist_;
+            match = match_;
+        }
+        public Song()
+        {
+        }
+        public string Title
+        {
+            get { return title; }
+        }
+        public string Artist
+        {
+            get { return artist; }
+        }
+        public int Match
+        {
+            get { return match; }
+        }
+    }
+}
