f84399b257340637dd5564a2772d1d8036d57da3
stephennancekivell <stephennancekivell@gmail.com>
2008-10-21 04:09:19 +0000

    Started a way to manage recordings, handling there not being a connection to the server etc.

--- World Hello/Program.cs
+++ World Hello/Program.cs
@@ -15,4 +15,32 @@ namespace World_Hello
             Application.Run(new SplashScreen());
         }
     }
+
+    class songinstance
+    {
+        string recordedwav;
+        DateTime recordtime;
+        string fingerprint;
+        SongList matches;
+
+        public songinstance()
+        {
+            recordtime = DateTime.Now;
+        }
+
+        public void record()
+        {
+            DynamicRecord.start();
+        }
+        public void stoprecord()
+        {
+            recordedwav= DynamicRecord.stop();
+            fingerprint = calcfinger.generate(recordedwav);
+        }
+        public bool askserver() // return true on succeed, return false on fail.
+        {
+            //sorry dont realy know what needs to be done here.
+            return false;
+        }
+    }
 }
